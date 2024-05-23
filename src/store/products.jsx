6b415c71f58d4f5a2972adb9f7cd232c8bfdaf12
lakshman1725 <kolapatilakshmansai@gmail.com>
2024-05-23 13:35:16 +0530

    ghjk

--- src/store/products.jsx
+++ src/store/products.jsx
@@ -3,19 +3,27 @@ import { connect } from "react-redux";
 function Products(props){
     console.log(props)
     return(
-        <div className="border border-warning m-2 p-2">
+        <div className="border border-warning m-2 p-2" style={{height:"200px", overflow:"scroll"}}>
             <ul className=" d-flex flex-wrap" >
-                {props.Productreducer.product.map((p)=>{
-                    return <li className="w-25 d-flex flex-column justify-content-between border border-success  p-5 m-2">
+                {props.product.map((p)=>{
+                    return <li className="w-25 d-flex flex-column justify-content-between border border-success  p-5 m-2 ">
                         <p>{p.title.slice(0,20)}</p>
                         <img src={p.image}  className="w-100" alt="" />
                         <h2>{p.price}</h2>
                         <h3>{p.brand}</h3>
-                        <button className="btn btn-warning" onClick={props.dispatch({type:"ADDTOCART",payload:p})}>Add to cart</button>
+                        <button className="btn btn-warning" onClick={props.cart(p)}>Add to cart</button>
                         </li>
                     })}
             </ul>
         </div>
     )
 }
-export default connect(store=>store)(Products)
+export default connect(function(state){
+    return state.Productreducer
+},function(dispatch){
+    return {
+        cart:(product)=>{
+            dispatch({type:"ADDTOCART",payload:Products})
+        }
+    }
+})(Products)
