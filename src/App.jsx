79d6400fab59779e5bfdcb47f9cedd4c0d8b6173
lakshman1725 <kolapatilakshmansai@gmail.com>
2024-05-25 12:26:22 +0530

    yuuyyyy

--- src/App.jsx
+++ src/App.jsx
@@ -9,14 +9,17 @@ function App(props) {
     
 
     <div className='border border-2 border-secondary m-2 p-2'>
-    <h1>
-             Cart:{props.Productreducer.cart.length}
-      </h1> 
+    
     <Link to="count">COUNTER</Link><br/>
      <Link to="todos">TODOLIST</Link><br/>
      <Link to="pro">PRODUCTS</Link><br/>
-     <Outlet></Outlet>
+     <h1>
+             Cart:{props.Productreducer.cart.length}
+      </h1> 
       
+      
+     <Outlet></Outlet>
+     
      </div>
       
   
