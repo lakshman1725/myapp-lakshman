0a64e921caba085633d17057c2bb900d1e60bbfc
lakshman1725 <kolapatilakshmansai@gmail.com>
2024-05-27 15:16:28 +0530

    ghgggg

--- src/app/store.js
+++ src/app/store.js
@@ -1,7 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit'
-import counterReducer from "../features/counter/counterSlice"
-import todoReducer from "../features/todolist/todolistSlice"
+
 export const store = configureStore({
-  reducers: {counter:counterReducer},
+  reducer: {},
 })
+
+
+
 
